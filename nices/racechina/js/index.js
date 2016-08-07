645d5d74ea3cbc0cbadc83f52fb369cebca6b7ff
zhouyuqin <zhouyoqin@yeah.net>
2016-08-07 22:52:49 +0800

    racechina2

--- nices/racechina/js/index.js
+++ nices/racechina/js/index.js
@@ -1,22 +1,8 @@
 var mainApp = angular.module("main",[]);
 mainApp.service('leancloud', function leancloud() {
     var ClassDefines = {
-      'BaseInfo': {attributes: [
-      		"nameEng", 
-			"nameChi" ,	
-			"lifeNumber", 
-			"microchipNumber",
-			"color", 	
-			"birthday", 	
-			"sex", 		
-			"sort", 		
-			"source", 	
-			"breed",
-			"passportFlag", 	
-			"status1", 	
-			"status2", 	
-			"importDate"]},
-      'ProductDetail': {attributes: ['size', 'price']}
+      'BaseInfo': {attributes: ["nameEng","nameChi","lifeNumber","microchipNumber","color", 	
+			"birthday", "sex", "sort", "source", "breed", "passportFlag", "status1", "status2", "importDate"]}
     };
     return {
       angularizeAll: function () {
@@ -41,18 +27,54 @@ mainApp.run(function(leancloud) {
 	AV.initialize("9yFukzmtOwmePyoc9GYjmHJ5-gzGzoHsz", "iUHln5KjOlOefVClzIDIEkXe");
 });
 mainApp.controller("mainController",function ($scope) {
-	$scope.horseInfo = [];
+  $scope.horseInfo = [];
+  $scope.pageStatus = false;
+  $scope.eachPageCount = 10;
 	$scope.queryList = function (){
-		debugger;
 		var query = new AV.Query('BaseInfo');
-		query.equalTo('birthday','2013-08-20');
-		query.find().then(function(res){
+		query.count().then(function(count){
 			$scope.$apply(function(){
-				$scope.horseInfo = res;
+        var sumPage = Math.ceil(count/$scope.eachPageCount);
+        getPage(1,count,sumPage);
 			});
 		}, function (err){
-
 		});
+	};
+
+  function getPage(curPage, sum, sumPage){
+    var queryPage = new AV.Query('BaseInfo');
+    queryPage.limit($scope.eachPageCount);
+    queryPage.skip($scope.eachPageCount * (curPage-1));
+    queryPage.find().then(function(res){
+      $scope.$apply(function(){
+        $scope.horseInfo = res;
+        generPageHtml(curPage, sum, sumPage)
+      });
+    });
+  };
+
+	function generPageHtml(curPage, sum, sumPage){
+      $('#kkpager').show();
+      kkpager.generPageHtml({
+          pno : curPage,
+          //总页码
+          total : sumPage,
+          //总数据条数
+          totalRecords : sum,
+          //链接前部
+          hrefFormer : '',
+          //链接尾部
+          hrefLatter : '',
+          getLink : function(n){
+              return 'javascript:;';
+          },
+          //,
+          mode : 'click',//默认值是link，可选link或者click
+          click : function(n){
+              // 翻页事件
+              getPage(n, sum, sumPage);
+          }
+      }, true);
 	};
 	$scope.quitUser = function() {
 		AV.User.logOut();
